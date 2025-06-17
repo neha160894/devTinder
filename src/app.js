af3b2330608a920a2ffc38ef5dad5ed08d97c968
Kashyap <neha.b.kashyap@accenture.com>
2025-06-17 14:47:38 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+    res.send("Hello from the test server!");
+})
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000...")
+});
