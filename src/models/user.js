53e6587d68820a2e40668fb792cab25abfe27ab8
Kashyap <neha.b.kashyap@accenture.com>
2025-06-23 18:00:23 +0530

    installed validator lib and added validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -14,11 +15,21 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address: " + value);
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Please enter a strong password: " + value);
+            }
+        }
     },
     age: {
         type: Number,
@@ -33,7 +44,12 @@ const userSchema = new mongoose.Schema({
         }
     },
     photoUrl: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid photo url: " + value);
+            }
+        }
     },
     about: {
         type: String,
