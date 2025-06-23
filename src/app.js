e2b4e8277d4b9becb66e261a4cf84f2919172ae8
Kashyap <neha.b.kashyap@accenture.com>
2025-06-23 16:18:28 +0530

    added delete/patch methods

--- src/app.js
+++ src/app.js
@@ -44,6 +44,28 @@ app.get("/feed", async (req, res) => {
     }
 });
 
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch (err) {
+        res.status(400).send("Something went wrong...")
+    }
+});
+
+// update data of the user
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({ _id: userId }, data);
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send("Something went wrong...")
+    }
+});
+
 connectDB().then(() => {
     console.log("Database connection established...");
     app.listen(3000, () => {
