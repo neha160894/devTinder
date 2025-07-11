c9a9687c561fe4d527a49d6a873106199d86aedb
Kashyap <neha.b.kashyap@accenture.com>
2025-07-11 13:22:28 +0530

    used express router to create routes and code cleanup in app.js

---
+++ src/routes/auth.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const authRouter = express.Router();
+
+const { validateSignUpData }  = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+authRouter.post("/signup", async (req, res) => {
+    try {
+        // validation of data
+        validateSignUpData(req);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        // encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        // creating a new instance of user model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        });
+        await user.save();
+        res.send("Data added successfully!");
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+authRouter.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid credentials!");
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+
+        if (isPasswordValid) {
+            // create a jwt token
+            const token = await user.getJWT();
+
+            // add the token to cookie and send the response bac to the user
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 * 3600000),
+            });
+            res.send("Login successful!");
+        } else {
+            throw new Error("Invalid credentials!");
+        }
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+module.exports = authRouter;
